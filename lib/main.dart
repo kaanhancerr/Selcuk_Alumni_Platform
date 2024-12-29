ac2a9ae62fdb6aa4e372bee1673c2243b0402d68
Kaan Hançer <kaannhancer@gmail.com>
2024-12-29 20:14:01 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:selcuk_alumni_platform/tanitim_sayfa.dart';
 import 'package:selcuk_alumni_platform/themes/colors_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:selcuk_alumni_platform/pages/giris_yap.dart';
+import 'package:selcuk_alumni_platform/pages/etkinlikler_sayfasi.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -14,8 +18,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const TanitimSayfa(),
+      home: const EtkinliklerSayfasi(),
       theme: colorsMode,
+      routes: {
+        '/giris': (context) => const GirisYap(),
+        '/etkinlikler': (context) => const EtkinliklerSayfasi(),
+      },
     );
   }
 }
